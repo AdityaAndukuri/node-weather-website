5eff2adc0072c4427c1622894dd69385fe6798e8
aditya_abhi123 <aditya.abhi123@gmail.com>
2019-11-02 15:18:19 +0530

    Added New Temperature stats

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback)=>{
         }else if(response.body.error){
             callback('Unable to find weather forecast of typed location, try other', undefined)
         }else{
-            callback(undefined, response.body.daily.data[0].summary+'Today temperature is '+response.body.currently.temperature+'C with '+response.body.currently.precipProbability+'% chance of rain')
+            callback(undefined, response.body.daily.data[0].summary+'Today temperature is '+response.body.currently.temperature+'C with '+response.body.currently.precipProbability+'% chance of rain\nMax Temperatuure is: '+response.body.daily.data[0].temperatureHigh+'\nMin Temperature is '+response.body.daily.data[0].temperatureLow)
         }
     })
 }
